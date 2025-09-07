a2f8bdf3f5f30011a9bd1be5f333182463722b4d
Maxim <besstrakhov.m@gmail.com>
2025-09-07 18:57:29 +0300

    first commit

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div>
+        Hello,i am home page
+    </div>
+  );
+}
---
+++ src/lib/app/store.ts
@@ -0,0 +1,25 @@
+import { configureStore,createSlice } from '@reduxjs/toolkit'
+
+
+
+
+const dummySlice = createSlice({
+    name: 'dummy',
+    initialState: {},
+    reducers: {},
+})
+
+
+export const store = configureStore({
+    reducer: {
+        dummy: dummySlice.reducer,
+    },
+})
+
+
+
+
+
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
