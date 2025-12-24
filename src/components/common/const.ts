16c29b21021919dd8fe7873b001a624bd80bd674
Maxim <besstrakhov.m@gmail.com>
2025-12-24 20:51:05 +0200

    update Login Form

--- src/components/common/const.ts
+++ src/components/common/const.ts
@@ -1,1 +1,3 @@
-export const baseUrl="http://localhost:8080/account"
+// export const baseUrl="http://localhost:8080/account"
+
+export const baseUrl="http://metrics-alb-1062964090.us-east-1.elb.amazonaws.com"
